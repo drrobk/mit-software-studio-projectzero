3bc47366f5bcfe193acff66a0316cf388db87365
Rob Kelly <robk@Robs-MacBook.local>
2014-06-25 00:25:35 +0100

    Created app for Project 0

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MitSoftwareStudioProjectzero::Application.config.secret_token = 'fa32846f2abbf385aed3108b0cd8802cc8f1b1e74f8a907d212d23d2fd9044b715216899b030fd19c53ec025ba00a428ffa029ef23577e45a1b599984eb23bec'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+MitSoftwareStudioProjectzero::Application.config.session_store :cookie_store, key: '_mit-software-studio-projectzero_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# MitSoftwareStudioProjectzero::Application.config.session_store :active_record_store
